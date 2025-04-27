940103f40471a37567f8d7e9e6ec297285e863ab
mdferdausalam19 <mdferdausalam333@gmail.com>
2025-04-27 16:02:21 +0600

    Add initial HTML and JavaScript files for Introduction section

---
+++ 01-introduction/first.js
@@ -0,0 +1,5 @@
+// Print a simple message to the console
+console.log("hello world!");
+
+// Find the div element by its ID and update its text content
+document.getElementById("div-id").innerText = "This is a div";
