5b9b2f0081f113b87687853c49f8525e8e9d942d
mdferdausalam19 <mdferdausalam333@gmail.com>
2025-05-03 11:07:19 +0600

    Add full loop concepts with comments and examples

--- 05-loops-and-iterations/index.js
+++ 05-loops-and-iterations/index.js
@@ -38,3 +38,107 @@ for (let i = 0; i < language.length; i++) {
   console.log(language.charAt(i)); // Output: J a v a S c r i p t (each on a new line)
   //   console.log(language[i]); // alternative way
 }
+
+// ----------------------
+// Nested Loop
+// ----------------------
+// A loop inside another loop. Useful for working with 2D structures like grids, tables, etc.
+for (let i = 1; i <= 3; i++) {
+  for (let j = 1; j <= 3; j++) {
+    console.log("row", i, "col", j);
+    // Output:
+    // row 1 col 1
+    // row 1 col 2
+    // ... up to row 3 col 3
+  }
+}
+
+// ----------------------
+// Break and Continue Statements
+// ----------------------
+
+// break: Exits the loop immediately when the condition is met
+for (let i = 1; i <= 5; i++) {
+  if (i === 3) {
+    break;
+  }
+  console.log(i); // Output: 1 2
+}
+
+// continue: Skips the current iteration and proceeds with the next one
+for (let i = 1; i <= 5; i++) {
+  if (i === 3) {
+    continue;
+  }
+  console.log(i); // Output: 1 2 4 5
+}
+
+// ----------------------
+// Multiple Counters in a Single Loop
+// ----------------------
+// Two variables incrementing and decrementing simultaneously
+for (let i = 1, j = 10; i <= 10 && j >= 1; i++, j--) {
+  console.log(i, j); // Output: 1 10, 2 9, ..., 10 1
+}
+
+// ----------------------
+// Star Pattern Printing
+// ----------------------
+/*
+ *
+ * *
+ * * *
+ * * * *
+ * * * * *
+*/
+
+// Method 01: Nested loop approach
+for (let row = 1; row <= 5; row++) {
+  let str = "";
+  for (let col = 1; col <= row; col++) {
+    str += "* ";
+  }
+  console.log(str);
+}
+
+// Method 02: Using string repeat
+for (let i = 1; i <= 5; i++) {
+  console.log("* ".repeat(i));
+}
+
+// ----------------------
+// While Loop
+// ----------------------
+// Runs as long as the condition is true. Best when the number of iterations is unknown in advance.
+let i = 1;
+while (i <= 5) {
+  console.log(i); // Output: 1 2 3 4 5
+  i++;
+}
+
+// ----------------------
+// Do-While Loop
+// ----------------------
+// Executes at least once, even if the condition is false initially
+let counter = 1;
+do {
+  console.log(counter); // Output: 1 2 3 4 5
+  counter++;
+} while (counter <= 5);
+
+// ----------------------
+// Infinite Loops (Do Not Run These!)
+// ----------------------
+/*
+for (;;) {
+  // console.log("I am looping forever!!!")
+}
+
+while (true) {
+  // console.log("I am looping forever!!!")
+}
+
+do {
+  // console.log("I am looping forever!!!")
+} while (true); 
+*/
